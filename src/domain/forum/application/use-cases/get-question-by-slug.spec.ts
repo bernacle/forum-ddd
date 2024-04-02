a05e3ef8eeab69e3c05387769ae62d362884d143
Bruno Simplicio <bruno@notabene.id>
2024-04-01 20:38:45 -0400

    creating factories for tests

--- src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -1,8 +1,7 @@
 import { InMemoryQuestionsRepository } from 'test/repositories/in-memory-questions-repository'
 import { GetQuestionBySlugUseCase } from './get-question-by-slug'
-import { Question } from '../../enterprise/entities/question'
+import { makeQuestion } from 'test/factories/make-question'
 import { Slug } from '../../enterprise/entities/value-objects/slug'
-import { UniqueEntityID } from '@/core/entities/unique-entity-id'
 
 let inMemoryQuestionsRepository: InMemoryQuestionsRepository
 let sut: GetQuestionBySlugUseCase
@@ -12,13 +11,9 @@ describe('Get Question By Slug', () => {
     inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
     sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
   })
+
   it('should be able to get a question by slug', async ({ expect }) => {
-    const newQuestion = Question.create({
-      authorId: new UniqueEntityID(),
-      title: 'Any Title',
-      content: 'Any Content',
-      slug: Slug.create('any-title'),
-    })
+    const newQuestion = makeQuestion({ slug: Slug.create('any-title') })
 
     await inMemoryQuestionsRepository.create(newQuestion)
 
