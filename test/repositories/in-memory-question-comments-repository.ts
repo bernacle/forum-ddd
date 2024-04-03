443ad507525007875d72bf9f02c0fb4242feba87
Bruno Simplicio <bruno@notabene.id>
2024-04-02 23:18:13 -0400

    delete question comment use case

--- test/repositories/in-memory-question-comments-repository.ts
+++ test/repositories/in-memory-question-comments-repository.ts
@@ -6,7 +6,22 @@ export class InMemoryQuestionCommentsRepository
 {
   public items: QuestionComment[] = []
 
+  async findById(id: string): Promise<QuestionComment | null> {
+    const questionComment = this.items.find((item) => item.id.toString() === id)
+
+    if (!questionComment) {
+      return null
+    }
+
+    return questionComment
+  }
+
   async create(questioncomment: QuestionComment): Promise<void> {
     this.items.push(questioncomment)
+  }
+
+  async delete(answer: QuestionComment): Promise<void> {
+    const index = this.items.findIndex((item) => item.id === answer.id)
+    this.items.splice(index, 1)
   }
 }
