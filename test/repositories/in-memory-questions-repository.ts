4b52dcb71e9dacdfd820768f63d9bc1e2f3c07a0
Bruno Simplicio <bruno@notabene.id>
2024-04-09 23:23:03 -0400

    new event for notifications

--- test/repositories/in-memory-questions-repository.ts
+++ test/repositories/in-memory-questions-repository.ts
@@ -1,3 +1,4 @@
+import { DomainEvents } from '@/core/events/domain-events'
 import { PaginationParams } from '@/core/repositories/pagination-params'
 import { QuestionAttachmentsRepository } from '@/domain/forum/application/repositories/question-attachments-repository'
 import { QuestionsRepository } from '@/domain/forum/application/repositories/questions-repository'
@@ -40,6 +41,8 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
 
   async create(question: Question): Promise<void> {
     this.items.push(question)
+
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 
   async delete(question: Question): Promise<void> {
@@ -54,5 +57,6 @@ export class InMemoryQuestionsRepository implements QuestionsRepository {
   async save(question: Question): Promise<void> {
     const index = this.items.findIndex((item) => item.id === question.id)
     this.items[index] = question
+    DomainEvents.dispatchEventsForAggregate(question.id)
   }
 }
